b5c1da82c8a2193862ad589ea61500d99c8f305f
Guruprasad Sridharan <guruprasad.sridharan@phonepe.com>
2017-11-26 00:41:04 +0530

    Project completed

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -7,6 +7,8 @@ from hmmlearn.hmm import GaussianHMM
 from sklearn.model_selection import KFold
 from asl_utils import combine_sequences
 
+from typing import Optional
+import traceback
 
 class ModelSelector(object):
     '''
@@ -14,7 +16,7 @@ class ModelSelector(object):
     '''
 
     def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
-                 n_constant=3,
+                 n_constant=3, n_folds=3,
                  min_n_components=2, max_n_components=10,
                  random_state=14, verbose=False):
         self.words = all_word_sequences
@@ -23,15 +25,16 @@ class ModelSelector(object):
         self.X, self.lengths = all_word_Xlengths[this_word]
         self.this_word = this_word
         self.n_constant = n_constant
+        self.n_folds = n_folds
         self.min_n_components = min_n_components
         self.max_n_components = max_n_components
         self.random_state = random_state
         self.verbose = verbose
 
-    def select(self):
+    def select(self) -> Optional[GaussianHMM]:
         raise NotImplementedError
 
-    def base_model(self, num_states):
+    def base_model(self, num_states) -> Optional[GaussianHMM]:
         # with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=DeprecationWarning)
         # warnings.filterwarnings("ignore", category=RuntimeWarning)
@@ -76,8 +79,55 @@ class SelectorBIC(ModelSelector):
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        competing_models = []
+        for n in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                model = self.base_model(n)
+                if model:
+                    competing_models.append(model)
+            except Exception as e:
+                if self.verbose:
+                    print(e)
+                pass
+
+        min_bic_score = np.inf
+        best_model = None
+        for model in competing_models:
+            try:
+                bic_score = self.calculate_bic_score(model.score(self.X, self.lengths),
+                                         self.no_of_free_parameters(model.n_components, len(self.X[0])),
+                                         len(self.X[:,0]))
+                if bic_score < min_bic_score:
+                    min_bic_score = bic_score
+                    best_model = model
+            except Exception as e:
+                if self.verbose:
+                    # traceback.print_exc()
+                    print(e)
+                pass
+        return best_model
+
+    def no_of_free_parameters(self, no_of_states: int, no_of_features: int) -> int:
+        '''
+        Calculates the number of free parameters (to be estimated) by model fitting
+        :param no_of_states: an integer 
+            Number of states of the HMM
+        :param no_of_features: an integer
+            Number of features used for training the HMM
+            
+        The formula for calculating number of free parameters is as follows
+            number_of_free_parameters(P) = initial_state_probabilities + transition_probabilities + emission_probability_gaussian_descriptors
+            => P = (no_of_states - 1) + (no_of_states * (no_of_states - 1)) + (no_of_features * no_of_states * 2) (1 + 1 for mean & SD)
+            => P = no_of_states ^ 2 - 1 + no_of_features * no_of_states * 2
+            
+        References:
+            1) https://discussions.udacity.com/t/number-of-parameters-bic-calculation/233235/11
+        :return: no_of_free_parameters
+        '''
+        return no_of_states ** 2 - 1 + (no_of_features * no_of_states * 2)
+
+    def calculate_bic_score(self, log_likelihood: float, no_of_parameters: int, no_of_data_points: int):
+        return -2 * log_likelihood + no_of_parameters * np.log(no_of_data_points)
 
 
 class SelectorDIC(ModelSelector):
@@ -92,8 +142,35 @@ class SelectorDIC(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        competing_models = []
+        for n in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                model = self.base_model(n)
+                if model:
+                    competing_models.append(model)
+            except Exception as e:
+                if self.verbose:
+                    print(e)
+                pass
+        all_words = list(self.words.keys())
+        all_other_words = [word for word in all_words if word != self.this_word]
+        max_dic_score = -np.inf
+        best_model = None
+        for model in competing_models:
+            try:
+                this_word_log_likelihood = model.score(self.X, self.lengths)
+                all_other_words_log_likelihood = [model.score(self.hwords[word][0], self.hwords[word][1]) for word in all_other_words]
+                avg_all_other_words_log_likelihood = np.mean(all_other_words_log_likelihood)
+                dic_score = this_word_log_likelihood - avg_all_other_words_log_likelihood
+                if dic_score > max_dic_score:
+                    max_dic_score = dic_score
+                    best_model = model
+            except (Exception, ValueError) as e:
+                if self.verbose:
+                    # traceback.print_exc()
+                    print(e)
+                pass
+        return best_model
 
 
 class SelectorCV(ModelSelector):
@@ -103,6 +180,33 @@ class SelectorCV(ModelSelector):
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        kf = KFold(n_splits=self.n_folds)
+        model_scores = []
+        for n in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                cur_model_log_likelihoods = []
+                model = None
+                if len(self.sequences) > self.n_folds:
+                    for train_index, test_index in kf.split(self.sequences):
+                        if self.verbose:
+                            print("Train fold indices:{} Test fold indices:{} No of components:{}".format(train_index, test_index, n))
+                        self.X, self.lengths = combine_sequences(train_index, self.sequences)
+                        test_X, test_lengths = combine_sequences(test_index, self.sequences)
+                        model = self.base_model(n)
+                        if model:
+                            log_likelihood = model.score(test_X, test_lengths)
+                            cur_model_log_likelihoods.append(log_likelihood)
+                    model_scores.append((model, np.mean(cur_model_log_likelihoods)))
+                else:
+                    model = self.base_model(n)
+                    model_scores.append((model, model.score(self.X, self.lengths)))
+            except Exception as e:
+                if self.verbose:
+                    # traceback.print_exc()
+                    print(e)
+                pass
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        if model_scores:
+            return sorted(model_scores, key = lambda x: x[1], reverse = True)[0][0]
+        else:
+            return None
--- my_recognizer.py
+++ my_recognizer.py
@@ -1,5 +1,6 @@
 import warnings
 from asl_data import SinglesData
+import numpy as np
 
 
 def recognize(models: dict, test_set: SinglesData):
@@ -20,6 +21,22 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    for word_idx in range(test_set.num_items):
+        word_probability = {}
+        max_log_likelihood = -np.inf
+        word_with_max_logl = None
+        for word, model in models.items():
+            Xlengths = test_set.get_item_Xlengths(word_idx)
+            log_likelihood = -np.inf
+            try:
+                log_likelihood = model.score(Xlengths[0], Xlengths[1])
+            except (Exception, ValueError) as e:
+                # skip over these words, don't consider scores for them
+                pass
+            word_probability[word] = log_likelihood
+            if log_likelihood > max_log_likelihood:
+                max_log_likelihood = log_likelihood
+                word_with_max_logl = word
+        probabilities.append(word_probability)
+        guesses.append(word_with_max_logl)
+    return probabilities, guesses
